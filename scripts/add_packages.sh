7763bb15bdcbff972fbbe41feb95b4e5cc139521
admin <admin@example.com>
2024-11-08 07:36:44 +0000

    content update

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -19,14 +19,14 @@ EOL
 
 # {{ Add luci-theme-argon
 (cd friendlywrt/package && {
-	[ -d luci-theme-argon ] && rm -rf luci-theme-argon
+	[[ -d luci-theme-argon ]] && rm -rf luci-theme-argon
 	git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 })
 echo "CONFIG_PACKAGE_luci-theme-argon=y" >> configs/rockchip/01-nanopi
-sed -i -e 's/function init_theme/function old_init_theme/g' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
+sed -i -e 's/function init_theme/function init_theme_backup/g' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
 cat > /tmp/appendtext.txt <<EOL
 function init_theme() {
-	if uci get luci.themes.Argon >/dev/null 2>&1; then
+	if uci get luci.themes.Argon > /dev/null 2>&1; then
 		uci set luci.main.mediaurlbase="/luci-static/argon"
 		uci commit luci
 	fi
